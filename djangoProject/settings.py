dcfea65bb30fb9c4ed531d883975f137ce64c4e0
Jeremy Hastings <jeremy.hastings@mac.com>
2024-09-28 14:32:57 -0500

    Add Static Files

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add this line to specify the directory for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
