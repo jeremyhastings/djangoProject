cdf88450be28ae06b64c30b20607bcd9d7e5e321
Jeremy Hastings <jeremy.hastings@mac.com>
2024-09-28 13:57:58 -0500

    Update to use env variables

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -15,17 +15,23 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import json
+import os
+
+# Load configuration from config.json
+with open(os.path.join(BASE_DIR, 'config.json')) as config_file:
+    config = json.load(config_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$0dbn7w6dpi=q$lfrky9nugz5f@(5ld3q62yx(6o6-w8qdtwy*'
+SECRET_KEY = os.getenv('SECRET_KEY', config.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', config.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config.get('ALLOWED_HOSTS')).split(',')
 
 
 # Application definition
